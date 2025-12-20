683faaf6ba1719c8050a009dac646f0d136576c5
Your Name <you@example.com>
2025-12-20 09:14:57 -0800

    primeira linha de código

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,9 @@
+from selenium import webdriver
+import time
 #Automação de encaminhamento de mensagens no whatsapp
 #Usando a Funcionalidade nativa do whatsapp de encaminhar menssagem
 #encaminhar de 5 em 5 menssagens
 
-
+#Abrir o navegador
+navegador = webdriver.Chrome() 
+time.sleep(10)
